ded1c451c9a904a0fe1f71bfb9e4fefd0ba011dd
captain-tab <d491405879@gmail.com>
2022-02-16 15:18:54 +0800

    feat: remove useless file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { IconStyle } from './assets/iconfont/iconfont'
 import React from 'react';
 import './App.css';
 import { GlobalStyle } from './style';
@@ -9,8 +6,6 @@ function App() {
   return (
       <div className="App">
           <GlobalStyle />
-          <IconStyle />
-          <i className="iconfont">&#xe62b;</i>
       </div>
   );
 }
